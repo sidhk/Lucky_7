0aeed0f4da7183cfe0b84313974a41da6c0a1f5c
sidharth  kumar <sidh@sidharths-MacBook-Pro.local>
2024-10-30 23:55:43 +0530

    Update basic UI and added destructuring

--- index.php
+++ index.php
@@ -9,14 +9,14 @@ function rollTheDices($type)
 {
     $dice1 = rand(1, 6);
     $dice2 = rand(1, 6);
-    $sumDice = $dice1 + $dice2;
+    $sumDices = $dice1 + $dice2;
     $profit = match ($type) {
-        0 => ($sumDice < 7) ? 20 : -10,
-        1 => ($sumDice == 7) ? 30 : -10,
-        2 => ($sumDice > 7) ? 20 : -10,
+        0 => ($sumDices < 7) ? 20 : -10,
+        1 => ($sumDices == 7) ? 30 : -10,
+        2 => ($sumDices > 7) ? 20 : -10,
         default => -10,
     };
-    return ['dice1' => $dice1, 'dice2' => $dice2, 'sum' => $sumDice, 'profit' => $profit];
+    return [$dice1, $dice2, $sumDices, $profit];
 }
 
 function getCurrentBalance($currbalance, $bet, $profit)
@@ -28,21 +28,46 @@ function getCurrentBalance($currbalance, $bet, $profit)
 if ($_POST['game_start'] ?? false) {
     $gameType = (int)$_POST['game_type'];
     $bet = (int)($_POST['game_bet'] ?? 10);
-    $diceInfo = rollTheDices($gameType);
-    $_SESSION['balance'] = getCurrentBalance($_SESSION['balance'], $bet, $diceInfo['profit']);
-    $message = $diceInfo['profit'] > 0 ? "Congratulations! You win!" : "Loss!";
+    [$dice1, $dice2, $sum, $profit] = rollTheDices($gameType);
+    $_SESSION['balance'] = getCurrentBalance($_SESSION['balance'], $bet, $profit);
+    $message = $profit > 0 ? "Congratulations! You win!" : "Loss!";
 }
 ?>
 <!DOCTYPE html>
 <html>
 <style>
+    body {
+        font-family: fantasy;
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 400% 400%;
+        animation: gradient 15s ease infinite;
+        height: 100vh;
+    }
+
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+
+        50% {
+            background-position: 100% 50%;
+        }
+
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+
     #container {
         width: 350px;
         padding: 20px;
-        border: 1px solid #ccc;
+        border: 1px solid #2e2e2e;
         margin: 100px auto;
         box-shadow: 2px 2px 10px #aaa;
         text-align: center;
+        box-shadow: 4px 4px 10px #c87979;
+        background-color: #edf4f3;
+        border-radius: 20px;
     }
 </style>
 
@@ -57,15 +82,15 @@ if ($_POST['game_start'] ?? false) {
                 <option value=2>Above 7</option>
             </select>
             <br />
-            <?php if (isset($diceInfo)): ?>
-                <p>Dice 1: <?= $diceInfo['dice1'] ?></p>
-                <p>Dice 2: <?= $diceInfo['dice2'] ?></p>
-                <p>Profit: <?= $diceInfo['profit'] ?></p>
-                <p>Sum: <?= $diceInfo['sum'] ?></p>
+            <?php if (isset($dice1)): ?>
+                <p>Dice 1: <?= $dice1 ?></p>
+                <p>Dice 2: <?= $dice2 ?></p>
+                <p>Profit: <?= $profit ?></p>
+                <p>Sum: <?= $sum ?></p>
                 <p><?= $message ?> Your balance is now <?= $_SESSION['balance'] ?> Rs</p>
             <?php endif; ?>
             <br />
-            <input type="submit" name="game_start" value="PLAY" style="width:30%;padding:10px;border-radius:8px;">
+            <input type="submit" name="game_start" value="PLAY" style="width:30%;padding:5px;border-radius:8px;">
             <br /> <br />
             <input type="submit" name="rest" value="Reset and Play again">
             <input type="submit" name="continue" value="Continue Playing">
